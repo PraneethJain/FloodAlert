de4d4721b31a4332616745dc0ea66d345d374559
PraneethJain <praneethjain005@gmail.com>
2023-06-13 17:56:26 +0530

    formatted

--- simulation/om2m.py
+++ simulation/om2m.py
@@ -10,18 +10,18 @@ headers = {
     'X-M2M-Origin': 'admin:admin',
     'Content-type': 'application/json'}
 while True:
-  sleep(2)
-  res1 = [eval(x['con']) for x in eval(requests.get(
-      f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
+    sleep(2)
+    res1 = [eval(x['con']) for x in eval(requests.get(
+        f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
 
-  res2 = [x['con']for x in eval(requests.get(
-      f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas2/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
+    res2 = [x['con']for x in eval(requests.get(
+        f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas2/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
 
-  res3 = [eval(x['con']) for x in eval(requests.get(
-      f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas3/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
+    res3 = [eval(x['con']) for x in eval(requests.get(
+        f"http://{ip}:5089/~/in-cse/in-name/{name}/Datas3/Values?rcn=4", headers=headers).text)["m2m:cnt"]["m2m:cin"]]
 
-  to_write = {"Demo": {"datas": res1, "datas2": res2, "datas3": res3}}
-  print(to_write)
+    to_write = {"Demo": {"datas": res1, "datas2": res2, "datas3": res3}}
+    print(to_write)
 
-  with open("./demo.json", "w") as f:
-      json.dump(to_write, f)
+    with open("./demo.json", "w") as f:
+        json.dump(to_write, f)
