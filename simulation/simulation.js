db736e9fe1eaf466e78ba8937970cbf5296edb51
PraneethJain <praneethjain005@gmail.com>
2023-06-13 11:50:20 +0530

    added ultrasonic and water level graph

--- simulation/simulation.js
+++ simulation/simulation.js
@@ -7,18 +7,30 @@ const main = async () => {
   let header = document.createElement("h1");
   header.textContent = "Flood #3";
   let page = document.createElement("div");
-  let graph_container = document.createElement("div");
-  graph_container.classList.add("container");
+  let graphs = document.createElement("div");
+  graphs.classList.add("graphs");
   let canvas1 = document.createElement("canvas");
   let canvas2 = document.createElement("canvas");
   let canvas3 = document.createElement("canvas");
   let canvas4 = document.createElement("canvas");
-
-  graph_container.append(canvas1, canvas2, canvas3, canvas4);
-  page.append(header, graph_container);
+  let c1 = document.createElement("div");
+  let c2 = document.createElement("div");
+  let c3 = document.createElement("div");
+  let c4 = document.createElement("div");
+  c1.classList.add("c");
+  c2.classList.add("c");
+  c3.classList.add("c");
+  c4.classList.add("c");
+  c1.appendChild(canvas1);
+  c2.appendChild(canvas2);
+  c3.appendChild(canvas3);
+  c4.appendChild(canvas4);
+  
+  graphs.append(c1, c2, c3, c4);
+  page.append(header, graphs);
   body.appendChild(page);
 
-  const floodData = data["Flood11"];
+  const floodData = data["Flood12"];
   const data1 = floodData["datas"];
   const data2 = floodData["datas2"];
   const data3 = floodData["datas3"];
@@ -43,13 +55,73 @@ const main = async () => {
   });
   ultrasonicReadings = data3.flat();
 
-  console.log(ultrasonicReadings);
-  console.log(temperatureReadings);
-  console.log(pressureReadings);
-  console.log(moistureReadings);
+  // console.log(ultrasonicReadings);
+  // console.log(temperatureReadings);
+  // console.log(pressureReadings);
+  // console.log(moistureReadings);
+  // console.log(waterLevelReadings);
+  // console.log(soilMoistureReadings);
+  // console.log(waterFlowReadings);
+
   console.log(waterLevelReadings);
-  console.log(soilMoistureReadings);
-  console.log(waterFlowReadings);
+  console.log(ultrasonicReadings);
+  new Chart(canvas1, {
+    type: "line",
+    data: {
+      labels: new Array(
+        Math.min(waterLevelReadings.length, ultrasonicReadings.length)
+      ).fill(0),
+      datasets: [
+        {
+          label: "Water Level Readings",
+          data: waterLevelReadings,
+          yAxisID: "y1",
+        },
+        {
+          label: "Ultrasonic Readings",
+          data: ultrasonicReadings,
+          yAxisID: "y2",
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      interaction: {
+        mode: "index",
+        intersect: false,
+      },
+      stacked: false,
+      scales: {
+        x: {
+          ticks: {
+            display: false,
+          },
+          grid: {
+            display: false,
+          },
+        },
+        y1: {
+          type: "linear",
+          display: true,
+          position: "left",
+          grid: {
+            display: false,
+          },
+        },
+        y2: {
+          type: "linear",
+          display: true,
+          position: "right",
+          grid: {
+            display: false,
+          },
+        },
+      },
+    },
+  });
+
+  
 };
 
 main();
