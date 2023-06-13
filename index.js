2181e1438de6b100dc614c520138c0bdd576ac66
PraneethJain <praneethjain005@gmail.com>
2023-06-13 17:12:44 +0530

    updating data live from om2m

--- index.js
+++ index.js
@@ -107,8 +107,233 @@ let floodProbability = new Chart(probabilities_ctx, {
   },
 });
 
+let graphs = document.createElement("div");
+graphs.classList.add("graphs");
+let canvas1 = document.createElement("canvas");
+let canvas2 = document.createElement("canvas");
+let canvas3 = document.createElement("canvas");
+let canvas4 = document.createElement("canvas");
+let c1 = document.createElement("div");
+let c2 = document.createElement("div");
+let c3 = document.createElement("div");
+let c4 = document.createElement("div");
+c1.appendChild(canvas1);
+c2.appendChild(canvas2);
+c3.appendChild(canvas3);
+c4.appendChild(canvas4);
 
-const fetchData = async () => {
+graphs.append(c1, c2, c3, c4);
+dataPage.appendChild(graphs);
+let chart1 = new Chart(canvas1, {
+  type: "line",
+  data: {
+    // labels: new Array(
+    //   Math.min(waterLevelReadings.length, ultrasonicReadings.length)
+    // ).fill(0),
+    datasets: [
+      {
+        label: "Water Level Readings",
+        // data: waterLevelReadings,
+        yAxisID: "y1",
+      },
+      {
+        label: "Ultrasonic Readings",
+        // data: ultrasonicReadings,
+        yAxisID: "y2",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    scales: {
+      x: {
+        ticks: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "left",
+        grid: {
+          display: false,
+        },
+      },
+      y2: {
+        type: "linear",
+        display: true,
+        position: "right",
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+let chart2 = new Chart(canvas2, {
+  type: "line",
+  data: {
+    // labels: new Array(
+    //   Math.min(soilMoistureReadings.length, moistureReadings.length)
+    // ).fill(0),
+    datasets: [
+      {
+        label: "Soil Moisture Readings",
+        // data: soilMoistureReadings,
+        yAxisID: "y1",
+      },
+      {
+        label: "Air Moisture Readings",
+        // data: moistureReadings,
+        yAxisID: "y2",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    scales: {
+      x: {
+        ticks: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "left",
+        grid: {
+          display: false,
+        },
+      },
+      y2: {
+        type: "linear",
+        display: true,
+        position: "right",
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+let chart3 = new Chart(canvas3, {
+  type: "line",
+  data: {
+    // labels: new Array(
+    //   Math.min(temperatureReadings.length, pressureReadings.length)
+    // ).fill(0),
+    datasets: [
+      {
+        label: "Temperature Readings",
+        // data: temperatureReadings,
+        yAxisID: "y1",
+      },
+      {
+        label: "Pressure Readings",
+        // data: pressureReadings,
+        yAxisID: "y2",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    scales: {
+      x: {
+        ticks: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "left",
+        grid: {
+          display: false,
+        },
+      },
+      y2: {
+        type: "linear",
+        display: true,
+        position: "right",
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+let chart4 = new Chart(canvas4, {
+  type: "line",
+  data: {
+    // labels: new Array(waterFlowReadings.length).fill(0),
+    datasets: [
+      {
+        label: "Water Flow Readings",
+        // data: waterFlowReadings,
+        yAxisID: "y",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    stacked: false,
+    scales: {
+      x: {
+        ticks: {
+          display: false,
+        },
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        type: "linear",
+        display: true,
+        position: "left",
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+
+const fetchDataStart = async () => {
   const response = await fetch("./simulation/demo.json");
   const data = await response.json();
   const floodData = data["Demo"];
@@ -149,248 +374,87 @@ const fetchData = async () => {
   // console.log(waterLevelReadings);
   // console.log(soilMoistureReadings);
   // console.log(waterFlowReadings);
-
-  let graphs = document.createElement("div");
-  graphs.classList.add("graphs");
-  let canvas1 = document.createElement("canvas");
-  let canvas2 = document.createElement("canvas");
-  let canvas3 = document.createElement("canvas");
-  let canvas4 = document.createElement("canvas");
-  let c1 = document.createElement("div");
-  let c2 = document.createElement("div");
-  let c3 = document.createElement("div");
-  let c4 = document.createElement("div");
-  c1.classList.add("c");
-  c2.classList.add("c");
-  c3.classList.add("c");
-  c4.classList.add("c");
-  c1.appendChild(canvas1);
-  c2.appendChild(canvas2);
-  c3.appendChild(canvas3);
-  c4.appendChild(canvas4);
-
-  graphs.append(c1, c2, c3, c4);
-  dataPage.appendChild(graphs);
-  new Chart(canvas1, {
-    type: "line",
-    data: {
-      labels: new Array(
-        Math.min(waterLevelReadings.length, ultrasonicReadings.length)
-      ).fill(0),
-      datasets: [
-        {
-          label: "Water Level Readings",
-          data: waterLevelReadings,
-          yAxisID: "y1",
-        },
-        {
-          label: "Ultrasonic Readings",
-          data: ultrasonicReadings,
-          yAxisID: "y2",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      interaction: {
-        mode: "index",
-        intersect: false,
-      },
-      stacked: false,
-      scales: {
-        x: {
-          ticks: {
-            display: false,
-          },
-          grid: {
-            display: false,
-          },
-        },
-        y1: {
-          type: "linear",
-          display: true,
-          position: "left",
-          grid: {
-            display: false,
-          },
-        },
-        y2: {
-          type: "linear",
-          display: true,
-          position: "right",
-          grid: {
-            display: false,
-          },
-        },
-      },
-    },
-  });
-
-  new Chart(canvas2, {
-    type: "line",
-    data: {
-      labels: new Array(
-        Math.min(soilMoistureReadings.length, moistureReadings.length)
-      ).fill(0),
-      datasets: [
-        {
-          label: "Soil Moisture Readings",
-          data: soilMoistureReadings,
-          yAxisID: "y1",
-        },
-        {
-          label: "Air Moisture Readings",
-          data: moistureReadings,
-          yAxisID: "y2",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      interaction: {
-        mode: "index",
-        intersect: false,
-      },
-      stacked: false,
-      scales: {
-        x: {
-          ticks: {
-            display: false,
-          },
-          grid: {
-            display: false,
-          },
-        },
-        y1: {
-          type: "linear",
-          display: true,
-          position: "left",
-          grid: {
-            display: false,
-          },
-        },
-        y2: {
-          type: "linear",
-          display: true,
-          position: "right",
-          grid: {
-            display: false,
-          },
-        },
-      },
-    },
-  });
-
-  new Chart(canvas3, {
-    type: "line",
-    data: {
-      labels: new Array(
-        Math.min(temperatureReadings.length, pressureReadings.length)
-      ).fill(0),
-      datasets: [
-        {
-          label: "Temperature Readings",
-          data: temperatureReadings,
-          yAxisID: "y1",
-        },
-        {
-          label: "Pressure Readings",
-          data: pressureReadings,
-          yAxisID: "y2",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      interaction: {
-        mode: "index",
-        intersect: false,
-      },
-      stacked: false,
-      scales: {
-        x: {
-          ticks: {
-            display: false,
-          },
-          grid: {
-            display: false,
-          },
-        },
-        y1: {
-          type: "linear",
-          display: true,
-          position: "left",
-          grid: {
-            display: false,
-          },
-        },
-        y2: {
-          type: "linear",
-          display: true,
-          position: "right",
-          grid: {
-            display: false,
-          },
-        },
-      },
-    },
-  });
-
-  new Chart(canvas4, {
-    type: "line",
-    data: {
-      labels: new Array(waterFlowReadings.length).fill(0),
-      datasets: [
-        {
-          label: "Water Flow Readings",
-          data: waterFlowReadings,
-          yAxisID: "y",
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      interaction: {
-        mode: "index",
-        intersect: false,
-      },
-      stacked: false,
-      scales: {
-        x: {
-          ticks: {
-            display: false,
-          },
-          grid: {
-            display: false,
-          },
-        },
-        y: {
-          type: "linear",
-          display: true,
-          position: "left",
-          grid: {
-            display: false,
-          },
-        },
-      },
-    },
-  });
 };
 
-fetchData();
+const fetchDataLoop = async () => {
+  const response = await fetch("./simulation/demo.json");
+  const data = await response.json();
+  const floodData = data["Demo"];
+  console.log(floodData);
+
+  const data1 = floodData["datas"];
+  const data2 = floodData["datas2"];
+  const data3 = floodData["datas3"];
+
+  let ultrasonicReadings = [];
+  let temperatureReadings = [];
+  let pressureReadings = [];
+  let moistureReadings = [];
+  let waterLevelReadings = [];
+  let soilMoistureReadings = [];
+  let waterFlowReadings = [];
+
+  data1.forEach(([a, b, c]) => {
+    waterLevelReadings.push(a);
+    soilMoistureReadings.push(b);
+    waterFlowReadings.push(c);
+  });
+
+  data2
+    .map((str) => str.replace(/nan/g, "0"))
+    .map(eval)
+    .forEach(([temp, press, moist]) => {
+      temperatureReadings.push(temp);
+      pressureReadings.push(press);
+      moistureReadings.push(moist);
+    });
+  ultrasonicReadings = data3.flat();
+
+  // console.log(ultrasonicReadings);
+  // console.log(temperatureReadings);
+  // console.log(pressureReadings);
+  // console.log(moistureReadings);
+  // console.log(waterLevelReadings);
+  // console.log(soilMoistureReadings);
+  // console.log(waterFlowReadings);
+
+  chart1.data.labels = new Array(
+    Math.min(waterLevelReadings.length, ultrasonicReadings.length)
+  ).fill("");
+  chart1.data.datasets[0].data = waterLevelReadings;
+  chart1.data.datasets[1].data = ultrasonicReadings;
+  chart1.update();
+
+  chart2.data.labels = new Array(
+    Math.min(soilMoistureReadings.length, moistureReadings.length)
+  ).fill("");
+  chart2.data.datasets[0].data = soilMoistureReadings;
+  chart2.data.datasets[1].data = moistureReadings;
+  chart2.update();
+
+  chart3.data.labels = new Array(
+    Math.min(temperatureReadings.length, pressureReadings.length)
+  ).fill(0);
+  chart3.data.datasets[0].data = temperatureReadings;
+  chart3.data.datasets[1].data = pressureReadings;
+  chart3.update();
+
+  chart4.data.labels = new Array(waterFlowReadings.length).fill("");
+  chart4.data.datasets[0].data = waterFlowReadings;
+  chart4.update();
+
+};
+
+setInterval(fetchDataLoop, 1000);
 
 function addData(chart, arr, datasetID = 0) {
-  let labels_arr = [];
-  for (let i = 0; i < arr.length; ++i)
-    labels_arr.push(arr[i][1].toLocaleTimeString());
-  chart.data.labels = labels_arr;
-  data_arr = [];
-  for (let i = 0; i < arr.length; ++i) data_arr.push(arr[i][0]);
-  chart.data.datasets[datasetID].data = data_arr;
+  // let labels_arr = [];
+  // for (let i = 0; i < arr.length; ++i)
+  //   labels_arr.push(arr[i][1].toLocaleTimeString());
+  // chart.data.labels = labels_arr;
+  // data_arr = [];
+  // for (let i = 0; i < arr.length; ++i) data_arr.push(arr[i][0]);
+  chart.data.datasets[datasetID].data = arr;
   chart.update();
 }
 
